b9cbb6d253bee7906e6a341d95e42f8afd197807
Justus Njuru Mbugua <justnjuru@gmail.com>
2023-03-08 21:09:40 +0300

    A starting base for the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import { useEffect, useState } from "react";
+
+import { Card } from "antd";
+const { Meta } = Card;
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bbf3efa2c0msh477e43d7009f0f6p1f0f7ejsn6020d990d89d",
+    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+  },
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { isLoaded, setIsloaded } = useState(false);
+  const { searchTitle, setsearchTitle } = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState();
+
+  const searchMovies = async (title) => {
+    try {
+      const response = await fetch(
+        `https://moviesdatabase.p.rapidapi.com/titles/search/akas/${title}`,
+        options
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      setIsloaded(true);
+      setError(err);
+      console.log(err);
+    }
+  };
+  searchMovies();
+  useEffect(() => {
+    searchMovies("Spiderman");
+
+    return () => {};
+  }, []);
+
+  if (error) {
+    return (
+      <div>
+        <p> Error: {error.message}</p>
+      </div>
+    );
+  } else if (!isLoaded) {
+    return (
+      <div>
+        <p>Loading....</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="app">
+        <h1>Movies</h1>
+        <input
+          placeholder="Search for movies"
+          value={searchTitle}
+          onChange={(e) => setsearchTitle(e.target.value)}
+        />
+        {movies.results.map((movie) => (
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              />
+            }
+          >
+            <Meta title="Europe Street beat" description="www.instagram.com" />
+          </Card>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
